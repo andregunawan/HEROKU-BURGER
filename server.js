ed3c74fbcec005f316db8762ff18452efaf4026b
Andre A Gunawan <andregunawan@users.noreply.github.com>
2017-11-21 23:25:39 -0800

    test 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const methodOverride = require('method-override');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,3 +26,5 @@ app.use('/', routes);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+// mysql://b5a219a5e5943e:5118e169@us-cdbr-iron-east-05.cleardb.net/heroku_e61d577eae314d0?reconnect=true
