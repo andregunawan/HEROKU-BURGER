dc117d67aa06ad3ada31a5de53d2394f8a650cd9
Andre A Gunawan <andregunawan@users.noreply.github.com>
2017-11-21 23:37:14 -0800

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const methodOverride = require('method-override');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
