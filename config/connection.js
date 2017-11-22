893765878a04856a3f489b968a8841bd825d4cdc
Andre A Gunawan <andregunawan@users.noreply.github.com>
2017-11-21 22:45:05 -0800

    herokuDB

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b5a219a5e5943e",
   password: "5118e169",
-  database: "burgers_db"
+  database: "heroku_e61d577eae314d0"
 });
 
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
--- INSERT INTO burgers (last_name,) VALUES ("", true);
+
 
 INSERT INTO burgers(burger_name, devoured, date) VALUES ("Double Cheeseburger", FALSE, CURRENT_TIMESTAMP);
 INSERT INTO burgers(burger_name, devoured, date) VALUES ("BLT Burger", FALSE, CURRENT_TIMESTAMP);
